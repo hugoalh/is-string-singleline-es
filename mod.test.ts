4d6aa3633ca5eb8658c4522899093a3427827a1d
hugoalh <hugoalh@gmail.com>
2025-04-08 18:29:33 +0800

    Update resources

--- mod.test.ts
+++ mod.test.ts
@@ -1,12 +1,12 @@
-import { assertEquals } from "STD/assert/equals";
+import { deepStrictEqual } from "node:assert";
 import { isStringSingleLine } from "./mod.ts";
 Deno.test("False 1", { permissions: "none" }, () => {
-	assertEquals(isStringSingleLine(`Wisi sed et at vero eos nostrud volutpat sed stet dignissim sit sanctus in eros.
+	deepStrictEqual(isStringSingleLine(`Wisi sed et at vero eos nostrud volutpat sed stet dignissim sit sanctus in eros.
 Et laoreet odio sanctus ea.
 Sea in dolores diam tincidunt labore sea stet vero dolor ut est.
 At aliquyam diam facilisis lorem et takimata et volutpat eros erat ipsum velit labore sed ea illum.
 Dolor lorem sed et volutpat exerci gubergren gubergren tempor quis ea eirmod eos ut dolor autem ipsum accumsan.`), false);
 });
 Deno.test("True 1", { permissions: "none" }, () => {
-	assertEquals(isStringSingleLine("Hello, world!"), true);
+	deepStrictEqual(isStringSingleLine("Hello, world!"), true);
 });
