a532f159254c03220df0b2d71acf8f60e0747f95
hugoalh <hugoalh@gmail.com>
2026-06-30 17:59:16 +0800

    Update resources

---
+++ .npm-codeberg.build.ts
@@ -0,0 +1,42 @@
+import { invokeDenoNodeJSTransformer } from "DNT";
+import { parse as parseJSONC } from "STD_JSONC";
+const jsrManifest = parseJSONC(await Deno.readTextFile(new URL(import.meta.resolve("./jsr.jsonc"))));
+await invokeDenoNodeJSTransformer({
+	copyEntries: [
+		"LICENSE.md",
+		"README.md"
+	],
+	//@ts-ignore Lazy type.
+	entrypointsScript: jsrManifest.exports,
+	generateDeclarationMap: true,
+	metadata: {
+		//@ts-ignore Lazy type.
+		name: jsrManifest.name,
+		//@ts-ignore Lazy type.
+		version: jsrManifest.version,
+		description: "A module to determine whether the string is single line.",
+		keywords: [
+			"is",
+			"line",
+			"single",
+			"singleline",
+			"string"
+		],
+		homepage: "https://codeberg.org/hugoalh/is-string-singleline-es#readme",
+		bugs: {
+			url: "https://codeberg.org/hugoalh/is-string-singleline-es/issues"
+		},
+		license: "MIT",
+		author: "hugoalh",
+		repository: {
+			type: "git",
+			url: "git+https://codeberg.org/hugoalh/is-string-singleline-es.git"
+		},
+		private: false,
+		publishConfig: {
+			access: "public"
+		}
+	},
+	outputDirectory: "dist/npm-codeberg",
+	outputDirectoryPreEmpty: true
+});
